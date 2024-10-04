9196c728796b42a7fe1f89291fa1c72711a7a21a
Eugenio Vazquez <lugezz@gmail.com>
2024-10-04 06:49:47 -0300

    Bases Diferenciales AOS y COS en Libro Sdo Digital

--- py_arg_reports/reporters/libro_sueldo_digital/generador_registros/registro4.py
+++ py_arg_reports/reporters/libro_sueldo_digital/generador_registros/registro4.py
@@ -4,9 +4,7 @@ from py_arg_reports.tools.tools import sync_format
 
 
 KEYS_CALCULOS_ESPECIFICOS = [
-    'base_calculo_diferencial_aporte_os',
     'base_calculo_diferencial_aporte_ss',
-    'base_calculo_diferencial_contribucion_os',
     'base_calculo_diferencial_contribucion_ss',
     'base_calculo_diferencial_lrt',
 ]
@@ -16,20 +14,10 @@ def process_keys_especificas(key_sp: str, info_931: dict) -> str:
     """ Procesa las claves específicas de los cálculos de la liquidación
         Para casos particulares que no vienen en Info F.931
     """
-    resp = ''
-    if key_sp == 'base_calculo_diferencial_aporte_os':
-        # El cálculo surge de Remuneracion4 - Remuneracion2
-        remuneracion4 = info_931['remuneracion_04']
-        remuneracion2 = info_931['remuneracion_02']
-        resp = str(remuneracion4 - remuneracion2)
-    elif key_sp == 'base_calculo_diferencial_contribucion_os':
-        # El cálculo surge de Remuneracion8 - Remuneracion2
-        remuneracion8 = info_931['remuneracion_08']
-        remuneracion2 = info_931['remuneracion_02']
-        resp = str(remuneracion8 - remuneracion2)
-    else:
-        # Todos los demás casos devuelven 0
-        resp = "0"
+    # Por el momento no cambia ninguno, se usaba para base diferencial de AOS y COS
+    # Pero finalmente se reporta en el campo correspondiente
+
+    resp = 0
 
     return resp
 
